6155956b671e70afb663b3196c63ac78b3a49c39
Jian Jin <jijin@ucdavis.edu>
2015-07-23 11:32:08 -0700

    initial commit

---
+++ R/GetEigenPlot.r
@@ -0,0 +1,13 @@
+GetNCluster = function(Ens){
+	
+	d = rowSums(Ens)
+	n = nrow(Ens)
+	Tmp = diag(d^(-1/2))
+	NormalizeEns = Tmp %*% Ens %*% Tmp
+	Eigenvalues = eigen(NormalizeEns)$values
+	COLOR = rep("black",n)
+	COLOR[which(Eigenvalues>0)] = "red"
+	plot(Eigenvalues, type = "b", pch = 20, col = COLOR,
+	cex = 0.5, ylab = "Normalized eigenvalues", main = "Eigen-plot")
+	
+}
