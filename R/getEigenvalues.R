14bd0a22bc92eb575e64da9602e222fd0a9ee31b
Jian Jin <jijin@ucdavis.edu>
2016-04-25 15:29:48 -0700

    remove examples for getEigenvaluesList. It is not exported for external use anymore. The process is automated and called from plotting functions. examples are not needed anymore.

--- R/getEigenvalues.R
+++ R/getEigenvalues.R
@@ -4,18 +4,19 @@
 #' @param EnsList a list of ensemble matrices
 #'
 #' @return a list of \code{eigenvalues} for each of the ensemble matrix in the ensemble matrices list.
-#' @examples
-#' Sim <- as.simMat(myData)
-#' temperatures <- temperatureSample(start = 0.01, end = 20, n = 20, method = 'random')
-#' \dontrun{
-#' # for illustration only. skip CRAN check because it ran forever.
-#' Ens_list <- getEnsList(Sim, temperatures, MaxIt = 1000, m = 5)
-#' }
-#' \dontshow{
-#' # for CRAN check only
-#' Ens_list <- getEnsList(Sim, temperatures, MaxIt = 5, m = 5)
-#' }
-#' eigenvalue_list <- getEigenvalueList(Ens_list)
+# #' @examples
+# #' Sim <- as.simMat(myData)
+# #' temperatures <- temperatureSample(start = 0.01, end = 20, n = 20, method = 'random')
+# #' \dontrun{
+# #' # for illustration only. skip CRAN check because it ran forever.
+# #' Ens_list <- getEnsList(Sim, temperatures, MaxIt = 1000, m = 5)
+# #' }
+# #' \dontshow{
+# #' # for CRAN check only
+# #' Ens_list <- getEnsList(Sim, temperatures, MaxIt = 5, m = 5)
+# #' }
+# #' eigenvalue_list <- getEigenvalueList(Ens_list)
+
 
 
 
