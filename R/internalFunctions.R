853921d63df945e39d5479fe7e42b6dad83d4e3f
Jian Jin <jijin@ucdavis.edu>
2016-04-27 13:15:42 -0700

    I don't think it is used anywhere. Try deleting this one and GetNCluster in next commit

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -1,4 +1,6 @@
 # helper function
+# I don't think this function is used.
+# Try deleting and run R CMD CHECK.
 vector2List <- function(vector){
   newList <- list()
   for (i in seq_along(vector)){
