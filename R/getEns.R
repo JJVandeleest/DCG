e70bde47677dcc5f3e9d9f3f2021f0999eb683fc
Jian Jin <jijin@ucdavis.edu>
2016-05-09 16:03:58 -0700

    update detail rationale in generating ensemble matrices

--- R/getEns.R
+++ R/getEns.R
@@ -1,11 +1,20 @@
 
-#' generate temperatures
+#' generating a list of ensemble matrices based on the similarity matrix and temperatures
 #' \code{getEnsList} get ensemble matrices from given similarity matrix at all temperatures
 #'
 #' @param simMat a similarity matrix
 #' @param temperatures temperatures selected
 #' @param MaxIt number of iterations for regulated random walks
 #' @param m maxiumnum number of time a node can be visited during random walks
+#'
+#' @details This step is crucial in finding community structure based on the similarity matrix of the social network.
+#' For each \code{temperatures}, the similarity matrix was taken to the power of \code{temperature} as saved as a new similarity matrix.
+#' This allows the random walk to explore the similarity matrix at various variations.
+#' Random walks are then performed in similarity matrices of various temperatures.
+#' In order to prevent random walks being stucked in a locale, the parameter \code{m} was set (to \code{5} by default) to remove a node after \code{m} times of visits of the node.
+#' An ensemble matrix is generated at each temperature in which values represent likelihood of two nodes being in the same community.
+#'
+#'
 #' @return a numeric vector of length n representing temperatures sampled.
 #'
 #' @examples
